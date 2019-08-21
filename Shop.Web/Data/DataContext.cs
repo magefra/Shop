860ad100104cfc4750dd4dd33fc2753c41a3c649
Magdiel <magdiel.palacios@csfacturacion.com>
2019-08-21 10:06:10 -0500

    Agregando Entities Product, migrations, Controller, View

---
+++ Shop.Web/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Shop.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Entities.Product> Products { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+        }
+
+
+    }
+}
---
+++ Shop.Web/Data/Entities/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Shop.Web.Data.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode =false)]
+        public decimal Price { get; set; }
+
+        [Display(Name= "Image")]
+        public string ImageUrl { get; set; }
+
+        [Display(Name = "Last Purchas")]
+        public DateTime LasPurchase { get; set; }
+
+        [Display(Name= "Last Sale")]
+        public DateTime LastSale { get; set; }
+
+        [Display(Name ="Is Availabe?")]
+        public bool IsAvailabe { get; set; }
+
+        [DisplayFormat(DataFormatString ="{0:N2}", ApplyFormatInEditMode = false)]
+        public double Stock { get; set; }
+
+
+    }
+}
