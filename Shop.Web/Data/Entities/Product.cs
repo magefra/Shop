142b3e705033ff89f66e98f2b54f7f66af8c8886
Magdiel <magdiel.palacios@csfacturacion.com>
2019-08-22 09:52:28 -0500

    add Seed Db

--- Shop.Web/Data/Entities/Product.cs
+++ Shop.Web/Data/Entities/Product.cs
@@ -6,24 +6,33 @@ namespace Shop.Web.Data.Entities
 {
     public class Product
     {
+
         public int Id { get; set; }
 
+        [MaxLength(50, ErrorMessage ="The field {0} only can contain {1} characters leng.")]
+        [Required]
         public string Name { get; set; }
+
 
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode =false)]
         public decimal Price { get; set; }
 
+
         [Display(Name= "Image")]
         public string ImageUrl { get; set; }
 
+
         [Display(Name = "Last Purchas")]
-        public DateTime LasPurchase { get; set; }
+        public DateTime? LasPurchase { get; set; }
+
 
         [Display(Name= "Last Sale")]
-        public DateTime LastSale { get; set; }
+        public DateTime? LastSale { get; set; }
+
 
         [Display(Name ="Is Availabe?")]
         public bool IsAvailabe { get; set; }
+
 
         [DisplayFormat(DataFormatString ="{0:N2}", ApplyFormatInEditMode = false)]
         public double Stock { get; set; }
---
+++ Shop.Web/Data/SeedDb.cs
@@ -0,0 +1,53 @@
+﻿using Shop.Web.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Web.Data
+{
+    /// <summary>
+    /// Clase que nos permite guardar registros a la DB si es que no contiene ningún registro.
+    /// </summary>
+    public class SeedDb
+    {
+        private readonly DataContext context;
+
+        private Random random;
+
+
+        public SeedDb(DataContext context)
+        {
+            this.context = context;
+            this.random = new Random();
+        }
+
+
+        public async Task SeedAsync()
+        {
+            await this.context.Database.EnsureCreatedAsync();
+
+            if (!this.context.Products.Any())
+            {
+                this.AddProduct("Iphone x");
+                this.AddProduct("Magic Maouse");
+                this.AddProduct("Iwatch Series 4");
+
+                await this.context.SaveChangesAsync();
+            }
+        }
+
+        private void AddProduct(string name)
+        {
+
+            this.context.Products.Add(new Product
+            {
+                Name = name,
+                Price = this.random.Next(100),
+                IsAvailabe = true,
+                Stock = this.random.Next(100)
+            }
+                );
+        }
+    }
+}
